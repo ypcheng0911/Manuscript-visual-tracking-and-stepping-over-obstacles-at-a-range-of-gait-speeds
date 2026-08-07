b62232d45ea1103cbc52aaff9f4fdb77e8602f02
Yu-Po Cheng <78725244+ypcheng0911@users.noreply.github.com>
2026-08-07 00:36:33 -0500

    Update Video_creation_gaze_and_object_detection.m

--- Computer_vision_model/Video_creation_gaze_and_object_detection.m
+++ Computer_vision_model/Video_creation_gaze_and_object_detection.m
@@ -1,6 +1,6 @@
 %%% Apply eye tracking trajectory on object detection video
 load('N01N01_10ms_125x_summary.mat')
-v = VideoReader("E:\Nordin_lab_data\Processed_data\Obstacle_avoidance\Eye_tracking_videos\N01\qgZdNuR91M7PeDbLGndGLg==.mp4\qgZdNuR91M7PeDbLGndGLg==.avi");
+v = VideoReader("<path_to_raw_data>\N01\qgZdNuR91M7PeDbLGndGLg==.mp4\qgZdNuR91M7PeDbLGndGLg==.avi");
 v2 = VideoWriter("testing_2.avi");
 open(v2)
 for i=1:v.NumFrames
