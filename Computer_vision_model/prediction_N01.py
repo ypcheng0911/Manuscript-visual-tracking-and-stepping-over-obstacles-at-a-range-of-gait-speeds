85302106bb9e4c86f493b260ecf1416b08d9e207
Yu-Po Cheng <78725244+ypcheng0911@users.noreply.github.com>
2026-08-07 00:38:21 -0500

    Update prediction_N01.py

--- Computer_vision_model/prediction_N01.py
+++ Computer_vision_model/prediction_N01.py
@@ -4,7 +4,7 @@ import glob
 model = YOLO("my_model.pt")
 
 # Retrive all video paths
-path = "E:\\Nordin_lab_data\\Raw_data\\Obstacle Avoidance Study raw data\\Eye tracking raw data\\N01\\Data\\Media"
+path = "<path_to_raw_data>\\N01\\Data\\Media"
 path_len = len(path)
 file_names = []
 video_paths = []
@@ -12,7 +12,7 @@ for files in glob.glob(path + '\\*.mp4'):
     video_paths.append(files)
     file_names.append(files[path_len+1:])
 
-save_path = "E:\\Nordin_lab_data\\Processed_data\\Obstacle_avoidance\\Eye_tracking_videos\\N01"
+save_path = "<path_to_processed_data>\\N01"
 # Iterate through all videos
 n = 0
 for cur_video in video_paths:
